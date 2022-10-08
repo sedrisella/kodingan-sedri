71131d308527c01039e5bd762850c27557c2f58b
sedrisella <114983194+sedrisella@users.noreply.github.com>
2022-10-08 17:40:16 +0700

    Add files via upload

---
+++ calsius sedri tugas 9.py
@@ -0,0 +1,10 @@
+print("""
+===============================
+PERHITUNGAN -REAMUR KE CALCIUS- 
+=============================== """)
+
+print("""Masukan nilai :""")
+
+r = float(input())
+c = float(5) / 4 * r
+print("Maka suhu dalam Celcius:" + str(c))
