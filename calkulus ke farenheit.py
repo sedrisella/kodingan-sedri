52983804c31b779b76adf54ad1e27e2f139d0d9d
sedrisella <114983194+sedrisella@users.noreply.github.com>
2022-10-08 17:37:23 +0700

    CALCIUS KE FARENHEIT

---
+++ calkulus ke farenheit.py
@@ -0,0 +1,13 @@
+print("""
+===============================
+PERHITUNGAN -CALCIUS KE REAMUR- 
+===============================""")
+
+print("""Masukan nilai yang anda mau :""")
+c = float(input())
+f = c * (float(9) / 5) + 32
+print("Maka suhu dalam Fahrenheit :" + str(f))
+print("""
+==============================
+TERIMA KASIH SUDAH MAMPIR
+==============================""")
