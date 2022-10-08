54cf6a1df2c9829bb793acde1badb0b58a10103c
sedrisella <114983194+sedrisella@users.noreply.github.com>
2022-10-08 17:35:09 +0700

    Add files via upload

---
+++ calcius ke reamur.py
@@ -0,0 +1,9 @@
+print("""
+===================================
+PERHITUNGAN -CALCIUS KE REAMUR- 
+=================================== """)
+
+print("""Masukan nilai yang anda mau :""")
+c = float(input())
+r = float(4) / 5 * c
+print("Maka suhu dalam reamur adalah :" + str(r))
