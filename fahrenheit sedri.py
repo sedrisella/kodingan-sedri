b39b0b89448a4df389e80ce85bf282b6d495dfe2
sedrisella <114983194+sedrisella@users.noreply.github.com>
2022-10-08 17:41:28 +0700

    Add files via upload

---
+++ fahrenheit sedri.py
@@ -0,0 +1,10 @@
+print("""
+===================================
+PERHITUNGAN -FAHRENHEIT KE CALCIUS- 
+=================================== """)
+
+print("""Masukan nilai yang anda mau :""")
+
+f = float(input())
+c = (f - 32) * (float(5) / 9)
+print("Maka suhu dalam Celcius adalah  :" + str(c))
